cf04316139fb23a70a81c3bd4dea1467f51208a6
arpitiiitv <arpitbittu143@gmail.com>
2018-09-06 23:16:15 +0530

    C program for sum of two numbers

---
+++ Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,z;
+    printf("Enter first number    :-  ");
+    scanf("%d",&x);
+     printf("Enter second number   :-  ");
+    scanf("%d",&y);
+    z=x+y;
+    printf("Sum of %d and %d is equals to %d \n",x,y,z);
+    return 0;
+}
